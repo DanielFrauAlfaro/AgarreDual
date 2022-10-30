abda5fe5bc720f47922cff5f6fd99310b7ee3828
DanielFrauAlfaro <98766327+DanielFrauAlfaro@users.noreply.github.com>
2022-10-30 10:23:21 +0100

    Eliminate /devel and /build

--- ur5/build/Universal_Robots_ROS_Driver/ur_calibration/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/daniel/Desktop/ur5/src/Universal_Robots_ROS_Driver/ur_calibration
-# Build directory: /home/daniel/Desktop/ur5/build/Universal_Robots_ROS_Driver/ur_calibration
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(_ctest_ur_calibration_gtest_calibration_test "/home/daniel/Desktop/ur5/build/catkin_generated/env_cached.sh" "/usr/bin/python3" "/opt/ros/noetic/share/catkin/cmake/test/run_tests.py" "/home/daniel/Desktop/ur5/build/test_results/ur_calibration/gtest-calibration_test.xml" "--return-code" "/home/daniel/Desktop/ur5/devel/lib/ur_calibration/calibration_test --gtest_output=xml:/home/daniel/Desktop/ur5/build/test_results/ur_calibration/gtest-calibration_test.xml")
-set_tests_properties(_ctest_ur_calibration_gtest_calibration_test PROPERTIES  _BACKTRACE_TRIPLES "/opt/ros/noetic/share/catkin/cmake/test/tests.cmake;160;add_test;/opt/ros/noetic/share/catkin/cmake/test/gtest.cmake;98;catkin_run_tests_target;/opt/ros/noetic/share/catkin/cmake/test/gtest.cmake;37;_catkin_add_google_test;/home/daniel/Desktop/ur5/src/Universal_Robots_ROS_Driver/ur_calibration/CMakeLists.txt;61;catkin_add_gtest;/home/daniel/Desktop/ur5/src/Universal_Robots_ROS_Driver/ur_calibration/CMakeLists.txt;0;")
--- ur5/build/myur5sim/myur5_moveit_config/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/daniel/Desktop/ur5/src/myur5sim/myur5_moveit_config
-# Build directory: /home/daniel/Desktop/ur5/build/myur5sim/myur5_moveit_config
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
