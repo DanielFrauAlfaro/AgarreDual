4919c4a55eaba34370d3e88f4f4034604bb6f87a
Daniel Frau Alfaro <98766327+DanielFrauAlfaro@users.noreply.github.com>
2022-11-14 16:41:53 +0100

    Trying to access the DH data

--- ur5/ik.py
+++ ur5/ik.py
@@ -6,15 +6,14 @@ import cv2 as cv
 import numpy as np
 
 ur5 = rtb.models.DH.UR5()
-print(ur5)
 
 q = [pi, -1.5, 1 , 0.0, 0.0, 0.0]
 T = ur5.fkine(q)
 
 sol = ur5.ikine_LM(T,q0 = q)
-print(T)
 
-traj = rtb.jtraj(ur5.qr, q, 100)
+
+# traj = rtb.jtraj(ur5.qr, q, 100)
 # ur5.plot(traj.q)
 
 # Ts = rtb.tools.trajectory.ctraj(T0, T1, len(t))
@@ -25,4 +24,6 @@ traj = rtb.jtraj(ur5.qr, q, 100)
 #sol = ur5.ikine_LM(T, ilimit=1000, rlimit=1000)  
 #print(sol)
 
-ur5.teach()
+# ur5.teach()
+
+ur5.DH
