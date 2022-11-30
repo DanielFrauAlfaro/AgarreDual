2518c00314b0d1cd3b4071df7c73fe1c21d911da
Daniel Frau Alfaro <98766327+DanielFrauAlfaro@users.noreply.github.com>
2022-11-30 20:28:53 +0100

    Feedback de imagen y reorganizar interfaz

--- ur5/main.py
+++ ur5/main.py
@@ -7,6 +7,12 @@ import roboticstoolbox as rtb
 import spatialmath as sm
 import rospy
 from geometry_msgs.msg import Pose
+from sensor_msgs.msg import Image
+
+from cv_bridge import CvBridge, CvBridgeError
+import cv2 as cv
+import numpy as np
+
 
 rospy.init_node("nodo")
 pub = rospy.Publisher("/pose", Pose, queue_size=10)
@@ -17,8 +23,6 @@ max_limit = 0.2
 min_limit = -0.2
 umbral = 0.01
 ready = False
-
-# Connect to ros master
 
 prev_x, prev_y, prev_z= 0.4919, 0.1092, 0.6578
 def callbackSlider(sender, app_data, user_data):
@@ -50,21 +54,49 @@ def callbackSlider(sender, app_data, user_data):
         prev_x = x
         prev_y = y
         prev_z = z
-        
 
+frame_ = Image()
+bridge = CvBridge()
+a = False
+def camera_cb(data):
+    global frame_, a, bridge
+    frame_ = bridge.imgmsg_to_cv2(data)
+    a = True
 
+rospy.Subscriber("/robot_camera/image_raw", Image, camera_cb)
 
-with dpg.window(tag="Primary Window"):
-    dpg.add_3d_slider(label="3D Slider", default_value=[0.4147, 0.1914, 0.6184], min_x=-0.6, max_x=0.6, min_y=-0.55, max_y=0.55, min_z=0.1, max_z=0.75, height=200, width=200, callback=callbackSlider, user_data="slider")
-    """ dpg.add_button(label="Save")
-    dpg.add_input_text(label="string", default_value="Quick brown fox")
-    dpg.add_slider_float(label="float", default_value=0.273, max_value=1) """
+while a==False:
+    b = 1
 
-dpg.create_viewport(title='Interfaz', width=600, height=600)
+dpg.create_context()
+dpg.create_viewport(title='Teleoperación', width=1460, height=800)
 dpg.setup_dearpygui()
+
+with dpg.window(tag="Controlador", width=800, height=800):
+    dpg.add_3d_slider(label="3D Slider", default_value=[0.4147, 0.1914, 0.6184], min_x=-0.6, max_x=0.6, min_y=-0.55, max_y=0.55, min_z=0.1, max_z=0.75, height=200, width=200,callback=callbackSlider, user_data="slider")
+
+
+data = np.flip(frame_, 2)  # because the camera data comes in as BGR and we need RGB
+data = data.ravel()  # flatten camera data to a 1 d stricture
+data = np.asfarray(data, dtype='f')  # change data type to 32bit floats
+texture_data = np.true_divide(data, 255.0)  # normalize image data to prepare for GPU
+
+
+with dpg.texture_registry(show=False):
+    dpg.add_raw_texture(frame_.shape[1], frame_.shape[0], texture_data, tag="texture_tag", format=dpg.mvFormat_Float_rgb)
+
+with dpg.window(label="Cámara UR5", pos = [800,0]):
+    dpg.add_image("texture_tag")
+
 dpg.show_viewport()
-dpg.set_primary_window("Primary Window", True)
-dpg.start_dearpygui()
-dpg.destroy_context()
+while dpg.is_dearpygui_running():
+    data = np.flip(frame_, 2)
+    data = data.ravel()
+    data = np.asfarray(data, dtype='f')
+    texture_data = np.true_divide(data, 255.0)
+    dpg.set_value("texture_tag", texture_data)
+
+    dpg.render_dearpygui_frame()
+
 
 # On ctrl+c client.stop()# pybullet
