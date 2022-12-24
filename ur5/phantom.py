7652eb3ca280cab0fdfda6c94cc61b4a225e8e1c
Daniel Frau Alfaro <98766327+DanielFrauAlfaro@users.noreply.github.com>
2022-12-24 13:11:57 +0100

    Add info on the phantom interface

--- ur5/phantom.py
+++ ur5/phantom.py
@@ -1,7 +1,8 @@
 #! /usr/bin/python3
 
+from control_msgs.msg import JointControllerState
 from geometry_msgs.msg import PoseStamped, Pose, WrenchStamped
-from sensor_msgs.msg import Joy, ImageCompressed
+from sensor_msgs.msg import Joy, CompressedImage
 from std_msgs.msg import Int32
 import rospy
 from cv_bridge import CvBridge
@@ -35,14 +36,14 @@ pose = Pose()
 wrench = WrenchStamped()
 
 # Mensajes para las imagenes
-frame_ = ImageCompressed()
-frame2_ = ImageCompressed()
+frame_ = CompressedImage()
+frame2_ = CompressedImage()
 bridge = CvBridge()
 
 # Factor de escala de los movimientos 
 scale_x = 2
 scale_y = 2
-scale_z1 = 0.4  # Diferente según está por encima o por debajo de 0
+scale_z1 = 3.5 # Diferente según está por encima o por debajo de 0
 scale_z2 = 7
 
 # Origen del robot
@@ -68,6 +69,9 @@ change = True           # Flag para indicar cambio (a True para mandarlo al (0,0
 first_frame_cam1 = False    # Flags para cuando llegue el primer frame de la cámara
 first_frame_cam2 = False
 
+# Posición articular
+q = [0, -1.5, 1 , 0.0, 1.57, 0.0]
+
 # Posición actual y previa del Phantom
 prev_pose_phantom = Pose()
 act_pose_phantom = Pose()
@@ -81,8 +85,34 @@ prev_pose_phantom.orientation.y = 0.0
 prev_pose_phantom.orientation.z = 0.0
 
 # Ganancia del feedback de fuerza
-K = 1
+K = 80
 limit = 0.001
+
+
+def shoulder_pan_listener(data):
+    global q   
+    q[0] = data.process_value
+        
+def shoulder_lift_listener(data):
+    global q
+    q[1] = data.process_value
+    
+def elbow_listener(data):
+    global q
+    q[2] = data.process_value
+    
+def wrist_1_listener(data):
+    global q
+    q[3] = data.process_value
+    
+def wrist_2_listener(data):
+    global q
+    q[4] = data.process_value
+    
+def wrist_3_listener(data):
+    global q
+    q[5] = data.process_value
+
 
 # Callbacks de las camaras
 def camera_cb(data):
@@ -227,6 +257,8 @@ def cb_bt1(data):
     if data.buttons[0] == 1:
         change = True
         
+        print("XYZ - RPY")
+        
         if xyz:
             xyz = False
         else:  
@@ -237,7 +269,7 @@ def cb_bt2(data):
     
     if data.buttons[0] == 1:
         change = True
-        
+        print("POS - VEL")
         if vel_control:
             vel_control = False
             pubMoveType.publish(0)  # Publica en el topic del robot para cambiar el modo de control 
@@ -259,49 +291,24 @@ rospy.Subscriber("/arm/measured_cp", PoseStamped,cb)
 rospy.Subscriber("/cart_pos", Pose, cart_cb)
 rospy.Subscriber("/arm/button1", Joy, cb_bt1)
 rospy.Subscriber("/arm/button2", Joy, cb_bt2)
-rospy.Subscriber("/robot_camera/image_raw/compressed", ImageCompressed, camera_cb)
-rospy.Subscriber("/robot_camera2/image_raw/compressed", ImageCompressed, camera_cb2)
+rospy.Subscriber("/robot_camera/image_raw/compressed", CompressedImage, camera_cb)
+rospy.Subscriber("/robot_camera2/image_raw/compressed", CompressedImage, camera_cb2)
+
+rospy.Subscriber('/shoulder_pan_joint_position_controller/state', JointControllerState, shoulder_pan_listener)
+rospy.Subscriber('/shoulder_lift_joint_position_controller/state', JointControllerState, shoulder_lift_listener)
+rospy.Subscriber('/elbow_joint_position_controller/state', JointControllerState, elbow_listener)
+rospy.Subscriber('/wrist_1_joint_position_controller/state', JointControllerState, wrist_1_listener)
+rospy.Subscriber('/wrist_2_joint_position_controller/state', JointControllerState, wrist_2_listener)
+rospy.Subscriber('/wrist_3_joint_position_controller/state', JointControllerState, wrist_3_listener)
 
 
 # Espera a los primeros frames de las cámaras 
 while first_frame_cam1 == False or first_frame_cam2 == False:
     pass
 
-'''
-dpg.create_context()
-dpg.create_viewport(title='Teleoperación', width=1460, height=1200)
-dpg.setup_dearpygui()
-
-
-data = np.flip(frame_, 2)  # because the camera data comes in as BGR and we need RGB
-data = data.ravel()  # flatten camera data to a 1 d stricture
-data = np.asfarray(data, dtype='f')  # change data type to 32bit floats
-texture_data = np.true_divide(data, 255.0)  # normalize image data to prepare for GPU
-
-data2 = np.flip(frame2_, 2)  # because the camera data comes in as BGR and we need RGB
-data2 = data2.ravel()  # flatten camera data to a 1 d stricture
-data2 = np.asfarray(data2, dtype='f')  # change data type to 32bit floats
-texture_data2 = np.true_divide(data2, 255.0)  # normalize image data to prepare for GPU
-
-
-with dpg.texture_registry(show=False):
-    dpg.add_raw_texture(frame_.shape[1], frame_.shape[0], texture_data, tag="texture_tag", format=dpg.mvFormat_Float_rgb)
-    dpg.add_raw_texture(frame2_.shape[1], frame2_.shape[0], texture_data2, tag="texture_tag2", format=dpg.mvFormat_Float_rgb)
-
-    
-with dpg.window(label="Cámara UR5", pos = [800,0]):
-    dpg.add_image("texture_tag")
-    
- 
-with dpg.window(label="Cámara UR52", pos = [800,500]):
-    dpg.add_image("texture_tag2")
-    
-
-dpg.show_viewport()'''
-
 
 # Rate
-r = rospy.Rate(10)
+r = rospy.Rate(20)
 
 # Bucle infinito
 '''
@@ -324,16 +331,17 @@ r = rospy.Rate(10)
 '''
 while not rospy.is_shutdown():
     # 1 --
-    if vel_control:
-        wrench.wrench.force.x = (0.0 - act_pose_phantom.position.x)*K
-        wrench.wrench.force.y = (0.0 - act_pose_phantom.position.y)*K
-        wrench.wrench.force.z = (0.0 - act_pose_phantom.position.z)*K
+    if not change:
+        if vel_control:
+            wrench.wrench.force.x = (0.0 - act_pose_phantom.position.x)*K
+            wrench.wrench.force.y = (0.0 - act_pose_phantom.position.y)*K
+            wrench.wrench.force.z = (0.0 - act_pose_phantom.position.z)*K
+            
         
-    
-    else:
-        wrench.wrench.force.x = 0.0
-        wrench.wrench.force.y = 0.0
-        wrench.wrench.force.z = 0.9
+        else:
+            wrench.wrench.force.x = 0.0
+            wrench.wrench.force.y = 0.9
+            wrench.wrench.force.z = 0.0
       
         
     # 2 --
@@ -349,6 +357,7 @@ while not rospy.is_shutdown():
                 
                 if (prev_pose_phantom.position.x - act_pose_phantom.position.x) < limit and (prev_pose_phantom.position.y - act_pose_phantom.position.y) < limit and (prev_pose_phantom.position.z - act_pose_phantom.position.z) < limit:
                     change = False
+                    print("########################################")
             
             # 2.1.2 --
             else:
@@ -372,29 +381,36 @@ while not rospy.is_shutdown():
     else:
         pub.publish(pose)
     
-    
     # 4 --
     pub_f.publish(wrench)
 
-    numpy_horizontal = np.hstack((frame_, frame2_))
     
-    cv2.imshow("UR5", numpy_horizontal)
-    cv2.waitKey(1)
+    mode = ""
     
-    # 5 --
-    '''data = np.flip(frame_, 2)
-    data = data.ravel()
-    data = np.asfarray(data, dtype='f')
-    texture_data = np.true_divide(data, 255.0)
-    dpg.set_value("texture_tag", texture_data)
+    if vel_control:
+        mode = "Velocidad"
+    else:
+        mode = "Posicion"
     
-    data2 = np.flip(frame2_, 2)
-    data2 = data2.ravel()
-    data2 = np.asfarray(data2, dtype='f')
-    texture_data2 = np.true_divide(data2, 255.0)
-    dpg.set_value("texture_tag2", texture_data2)
+    if xyz:
+        mode = mode + "XYZ"
+    else:
+        mode = mode + "RPY"
+    
+    zeros = cv2.putText(zeros, "Modo: " + mode, (15,50), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (128, 0, 128), 2, cv2.LINE_AA)
+    zeros = cv2.putText(zeros, "X: " + str(round(prev_x,2)) + "   Y: " + str(round(prev_y,2)) + "   Z: " + str(round(prev_z,2)), (15,100), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (128, 128, 0), 1, cv2.LINE_AA)
+    zeros = cv2.putText(zeros, "Roll: " + str(round(prev_roll,2)) + "  Pitch: " + str(round(prev_pitch,2)) + "  Yaw: " + str(round(prev_yaw,2)), (15,150), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (128, 128, 0), 1, cv2.LINE_AA)
+    
+    zeros = cv2.putText(zeros, "Q0: " + str(round(q[0],2)), (15,200), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 128, 128), 1, cv2.LINE_AA)
+    zeros = cv2.putText(zeros, "Q1: " + str(round(q[1],2)), (15,220), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 128, 128), 1, cv2.LINE_AA)
+    zeros = cv2.putText(zeros, "Q2: " + str(round(q[2],2)), (15,240), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 128, 128), 1, cv2.LINE_AA)
+    zeros = cv2.putText(zeros, "Q3: " + str(round(q[3],2)), (15,260), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 128, 128), 1, cv2.LINE_AA)
+    zeros = cv2.putText(zeros, "Q4: " + str(round(q[4],2)), (15,280), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 128, 128), 1, cv2.LINE_AA)
+    zeros = cv2.putText(zeros, "Q5: " + str(round(q[5],2)), (15,300), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 128, 128), 1, cv2.LINE_AA)
 
-    dpg.render_dearpygui_frame()'''
     
+    numpy_horizontal = np.hstack((frame_, frame2_, zeros))
+    cv2.imshow("UR5", numpy_horizontal)
     
+    cv2.waitKey(1)
     r.sleep()
