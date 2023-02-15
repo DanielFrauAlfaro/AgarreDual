e29eaf8d11489d11d833d3e6d517763a59389611
Daniel Frau Alfaro <danielfraualfa@gmail.com>
2023-02-15 20:00:14 +0100

    Update slider.py

--- slider.py
+++ slider.py
@@ -46,6 +46,7 @@ frame_ = CompressedImage()
 frame2_ = CompressedImage()
 bridge = CvBridge()
 
+pose = Pose()
 
 interval = 0.1
 prev = time.time()
@@ -102,7 +103,7 @@ def callbackSlider(sender, app_data, user_data):
     global prev_x, prev_roll
     global prev_y, prev_pitch
     global prev_z, prev_yaw
-    global pub, interval, prev
+    global pub, interval, prev, pose
     
     if time.time() - prev > interval:         # Solo se ejecuta cuando pasa el intervalo
             
@@ -116,10 +117,6 @@ def callbackSlider(sender, app_data, user_data):
         if abs(x-prev_x) > umbral or abs(y-prev_y) > umbral or abs(z-prev_z) > umbral:
             
             pose = Pose()
-            
-            prev_x = x
-            prev_y = y
-            prev_z = z
 
             pose.position.x = x
             pose.position.y = y
@@ -129,6 +126,10 @@ def callbackSlider(sender, app_data, user_data):
             pose.orientation.y = prev_pitch
             pose.orientation.z = prev_yaw
             pose.orientation.w = 1
+
+            prev_x = x
+            prev_y = y
+            prev_z = z
             
             pub.publish(pose)
 
@@ -149,10 +150,6 @@ def callbackSlider2(sender, app_data, user_data):
         z = data[2]
         if abs(x-prev_x) > umbral or abs(y-prev_y) > umbral or abs(z-prev_z) > umbral:
             pose = Pose()
-
-            prev_roll = x
-            prev_pitch = y
-            prev_yaw = z
                 
             pose.position.x = prev_x
             pose.position.y = prev_y
@@ -165,96 +162,43 @@ def callbackSlider2(sender, app_data, user_data):
                 
             pub.publish(pose)
 
-# XYZ velocidad: se hacen por incrementos de XYZ, por lo que los RPY estan a 0.0
-def callbackSlider_(sender, app_data, user_data):
-    global prev_x, prev_roll
-    global prev_y, prev_pitch
-    global prev_z, prev_yaw
-    global pub, prev3, interval3
-    
-    if time.time() - prev3 > interval3:         # Solo se ejecuta cuando pasa el intervalo
-            
-        prev3 = time.time() 
-        data = dpg.get_value(sender)
-        x = data[0]
-        y = data[1]
-        z = data[2]
-        
-        pose = Pose()
-        
-        pose.position.x = x
-        pose.position.y = y
-        pose.position.z = z
 
-        pose.orientation.x = 0.0
-        pose.orientation.y = 0.0
-        pose.orientation.z = 0.0
-        pose.orientation.w = 0.0
-        
-        pub.publish(pose)
 
-# RPY velocidad: se hacen incrementos de RPY, por lo que los XYZ estan a 0.0
-def callbackSlider2_(sender, app_data, user_data):
-    global prev_x, prev_roll
-    global prev_y, prev_pitch
-    global prev_z, prev_yaw
-    global pub, prev4, interval4
+def cart_pos(data):
+    global prev_x, prev_y, prev_z
+    global prev_roll, prev_pitch, prev_yaw
 
-    if time.time() - prev4 > interval4:         # Solo se ejecuta cuando pasa el intervalo
-            
-        prev4 = time.time() 
-        data = dpg.get_value(sender)
-        x = data[0]
-        y = data[1]
-        z = data[2]
-        
-        pose = Pose()
-                
-        pose.position.x = 0.0
-        pose.position.y = 0.0
-        pose.position.z = 0.0
-
-        pose.orientation.x = x
-        pose.orientation.y = y
-        pose.orientation.z = z
-        pose.orientation.w = 1
-                
-        pub.publish(pose)
-
+    if False:
+        prev_x = data.position.x
+        prev_y = data.position.y
+        prev_z = data.position.z
+    if False:
+        prev_roll = data.orientation.x
+        prev_pitch = data.orientation.y
+        prev_yaw = data.orientation.z
 
 # Variables de ROS:
 # Nodo
 
-name = "ur5_1"
+name = "ur5_2"
 rospy.init_node("nodo_"+name)
 
 # Publosher de la pose y el modo de movimiento
 pub = rospy.Publisher("/"+name+"/pose", Pose, queue_size=10)
 pubMoveType = rospy.Publisher("/move_type", Int32, queue_size=10) 
 
-# Subscribers de la pose del robot y las dos cámaras
-rospy.Subscriber("/robot_camera/image_raw/compressed", CompressedImage, camera_cb)
-rospy.Subscriber("/robot_camera2/image_raw/compressed", CompressedImage, camera_cb2)
+rospy.Subscriber("/" + name + "/cart_pos", Pose, cart_pos)
 
 
 # GUI
 dpg.create_context()
-dpg.create_viewport(title='Teleoperación', width=1460, height=1200)
+dpg.create_viewport(title='Teleoperación', width=700, height=600)
 dpg.setup_dearpygui()
 
 # Se crean los sliders en las posiciones iniciales
-with dpg.window(tag="Controlador", width=800, height=640):
-    dpg.add_3d_slider(label="Position XYZ", tag="position_slider", default_value=[0.5095, 0.1334, 0.7347], min_x=-0.7, max_x=0.7, min_y=-0.55, max_y=0.55, min_z=0.0, max_z=0.85, height=200, width=200,callback=callbackSlider, user_data="slider", )
+with dpg.window(tag="Controlador", width=600, height=600):
+    dpg.add_3d_slider(label="Position XYZ", tag="position_slider", default_value=[0.5095, 0.1334, 0.7347], min_x=-1, max_x=0.7, min_y=-0.55, max_y=0.55, min_z=0.0, max_z=0.85, height=200, width=200,callback=callbackSlider, user_data="slider", )
     dpg.add_3d_slider(label="Position RPY", tag="rotation_slider", default_value=[1.57225399 , 1.07079575, -0.001661], min_x=-3.14, max_x=3.14, min_y=-3.14, max_y=3.14, min_z=-3.14, max_z=3.14, height=200, width=200,callback=callbackSlider2, user_data="slider")
-
-    dpg.add_3d_slider(label="Velocity XYZ", show=False, tag="position_slider_v", default_value=[0.0, 0.0, 0.0], min_x=-0.1, max_x=0.1, min_y=-0.1, max_y=0.1, min_z=-0.1, max_z=0.1, height=200, width=200,callback=callbackSlider_, user_data="slider")
-    dpg.add_3d_slider(label="Velocity RPY", show = False, tag="rotation_slider_v", default_value=[0.0, 0.0, 0.0], min_x=-0.1, max_x=0.1, min_y=-0.1, max_y=0.1, min_z=-0.1, max_z=0.1, height=200, width=200,callback=callbackSlider2_, user_data="slider")
-
-
-# Se crea el checker para el cambio de modo
-with dpg.window(tag="Ckeck", pos=[0,640]):
-    dpg.add_checkbox(label="Activar control en velocidad", tag="vel_control",
-                            callback=callbackCheck, user_data="vel_control")
 
 
 
@@ -282,6 +226,8 @@ while dpg.is_dearpygui_running():
             dpg.configure_item("rotation_slider", show=True, label="Position RPY", default_value=[prev_roll, prev_pitch, prev_yaw])
         
         cb = False
+
+
 
     r.sleep()
     
