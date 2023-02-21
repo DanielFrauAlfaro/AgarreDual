dd4e671585d8cb4af592fd0ac0f3b679e998c2b8
Daniel Frau Alfaro <danielfraualfa@gmail.com>
2023-02-21 20:05:50 +0100

    Wrist torque topics and wrist joint feedback

--- src/nodes/scripts/cart_pos.py
+++ src/nodes/scripts/cart_pos.py
@@ -1,6 +1,6 @@
 #! /usr/bin/python3
 
-from std_msgs.msg import Float32MultiArray
+from std_msgs.msg import Float32MultiArray, Bool
 from geometry_msgs.msg import Pose, WrenchStamped
 from sensor_msgs.msg import JointState
 import rospy
@@ -24,27 +24,26 @@ ur5 = rtb.DHRobot([
 
 ur5.base = SE3.RPY(0,0,pi)      # Rotate robot base so it matches Gazebo model
 
-
 # Posición articular
 q = [0, -1.5, 1 , 0.0, 1.57, 0.0]
 qd = [0, 0, 0, 0, 0, 0]
 tau = [0, 0, 0, 0, 0, 0]
 
-# Posiciones previas para XYZ y RPY
-#   Para al cambiar de XYZ a RPY y viceversa se guarden las XYZ o RPY segun convenga en el mensaje de pose
-prev_x, prev_y, prev_z = 0.5095, 0.1334, 0.7347
-prev_roll, prev_pitch, prev_yaw = 1.57225, 1.07, -0.00166
-
 # Mensajes a enviar
 p = Pose()
 g = Float32MultiArray()
 f = WrenchStamped()
+msg_wrist_torque = Float32MultiArray()
+
 
 # Tiempo para coger los mensajes del /joint_states
 interval = 0.2
 prev = time.time()
 
-# Nombre
+# Flag de la orientación
+orientation = False
+
+# Nombres
 name = "ur5_2"
 grip = "2f_140"
 
@@ -56,6 +55,7 @@ prev_grip_3f_1, prev_grip_3f_2, prev_grip_3f_mid, prev_grip_3f_palm = 0, 0, 0, 0
 pubs = []
 
 
+# Callbacks de los sensores de par de cada articulación
 def torque_cb_shoulder_pan(data):
     global tau
 
@@ -91,12 +91,14 @@ def torque_cb_wrist_3(data):
 # Estado de las articulaciones
 def joint_state_cb(data):
     global q, qd, prev, interval
-    global p, g, f, pubs
+    global p, g, f, msg_wrist_torque, pubs
     global prev_grip_140
     global name, grip
     global tau
-
-    if time.time() - prev > interval:         # Solo se ejecuta cuando pasa el intervalo
+    global orientation
+    
+    # Cuando pasa el intervalo de tiempo se ejecuta el feedback
+    if time.time() - prev > interval:        
         
         # Obtiene los valores articulares del robot
         for i in range(len(data.name)):
@@ -140,14 +142,31 @@ def joint_state_cb(data):
                 '''elif data.name[i] == "gripper_palm_finger_1_joint":
                     prev_grip_3f_palm = data.position[i]'''
 
+
         # Cinemática directa (CD)
         T = ur5.fkine(q, order='xyz')
+
+        # Obtiene los valores de traslación y rotación en ángulos de Euler
+        trans = T.t
+        eul = T.rpy(order='xyz')
+
+        # Construye el mensaja para la posición    
+        p.position.x = round(trans[0],3)
+        p.position.y = round(trans[1],3)
+        p.position.z = round(trans[2],3)
+
+        p.orientation.x = q[3]
+        p.orientation.y = q[4]
+        p.orientation.z = q[5]
+
+
+        # Jacobiana directa
         J = ur5.jacob0(q, T)
 
-        # J_ = np.linalg.inv(J)
-        J_ = J.transpose()
+        # Fuerza cartesiana en el extremo
         f_ = np.matmul(J, tau)
-        
+
+        # Construye el mensaje para la fuerza        
         f.wrench.force.x = f_[0]
         f.wrench.force.y = f_[1]
         f.wrench.force.z = f_[2]
@@ -156,25 +175,7 @@ def joint_state_cb(data):
         f.wrench.torque.y = f_[4]
         f.wrench.torque.z = f_[5]
 
-        '''np.set_printoptions(precision=2)
-        print (f)
-        print("-------------------------------")
-        print("")'''
-
-        # Obtiene los valores de traslación y rotación en ángulos de Euler
-        trans = T.t
-        eul = T.rpy(order='xyz')
-
-        # Obtiene las traslaciones lineales y angulares
-        
-        p.position.x = round(trans[0],3)
-        p.position.y = round(trans[1],3)
-        p.position.z = round(trans[2],3)
-
-        p.orientation.x = round(eul[0], 3)
-        p.orientation.y = round(eul[1], 3)
-        p.orientation.z = round(eul[2], 3)
-
+        # En función del nombre crea unos mensajes para el gripper
         if grip == "2f_140":
             g_aux = [prev_grip_140]
 
@@ -184,17 +185,27 @@ def joint_state_cb(data):
             g_aux = [prev_grip_3f_1, prev_grip_3f_2, prev_grip_3f_mid, prev_grip_3f_palm]
 
             g.data = g_aux
+        
+        msg_wrist_torque.data = np.array([tau[3], tau[4], tau[5]])
 
+        # Publica los mensajes
         pubs[0].publish(p)
         pubs[1].publish(g)
-        pubs[2].publish(f)
+        pubs[2].publish(f)        
+        pubs[3].publish(msg_wrist_torque)
 
         # Actualiza el tiempo
         prev = time.time()
 
 
+# Callback para el modo en orientación
+def orientation_cb(data):
+    global orientation
+
+    orientation = data
 
 
+# Main
 if __name__ == '__main__':
 
     if len(sys.argv) > 0:
@@ -220,6 +231,6 @@ if __name__ == '__main__':
             pubs.append(rospy.Publisher("/" + name + '/grip_pos', Float32MultiArray, queue_size=10))
 
         pubs.append(rospy.Publisher("/" + name + "/cart_force", WrenchStamped, queue_size=10))
-
+        pubs.append(rospy.Publisher("/" + name + "/wrist_torque", Float32MultiArray, queue_size=10))
 
         rospy.spin()
