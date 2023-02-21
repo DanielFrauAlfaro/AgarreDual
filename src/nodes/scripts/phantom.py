9435d83d4bd7fa879bd5011727595bbee575ebd6
Daniel Frau Alfaro <danielfraualfa@gmail.com>
2023-02-21 20:06:12 +0100

    Orientation mode messages

--- src/nodes/scripts/phantom.py
+++ src/nodes/scripts/phantom.py
@@ -1,16 +1,13 @@
 #! /usr/bin/python3
 
-from std_msgs.msg import Float64, Int32, Float32MultiArray
+from std_msgs.msg import Float64, Int32, Float32MultiArray, Bool
 from geometry_msgs.msg import PoseStamped, WrenchStamped, Pose
 from sensor_msgs.msg import Joy
 import rospy
 import sys
 import time
+import numpy as np
 
-# Button: sensor_msgs/Joy /arm/button1    /arm/button2
-
-# Wrench: geometry_msgs/WrenchStamped   /arm/servo_cf
-# Fuerza: y = 0.9
 '''
 x_limit: 0.09 - -0.07           * 2
 y_limit: 0.2                    * 2
@@ -21,46 +18,57 @@ z_limit: 0.19  - -0.075         * 0.4 // *7
 OBJETIVO
     El Phantom presenta 5 GDL pero solo 3 son actuables para el feedback de fuerza. Por ello, se mueve en XYZ con las coordenadas XYZ del Phantom
 por defecto.
-    Al presionar un botón, se cambia el modo de movimiento; las coordenadas RPY del robot se mueven con las coordenadas XYZ del Phantom.
-    
-    Al presionar el otro botón se cambia el modo de control de posición a velocidad o viceversa, con el mismo modus operandi respecto al modo de
-movimiento.
+
+    Hay tres estados en los que se puede mover el robot:
+        - Estado 0: Posición XYZ en el espacio
+        - Estado 1: Control articular de la muñeca del robot
+        - Estado 2: Control de la pinza
+            · Estado 2.1: Control de los dedos
+            · Estado 2.2: Control de la palma
+
+    Las transiciones se realizan con los botones del Phantom, de manera que con el botón 1 se avanza el estado y con el botón
+2 se retrocede.
+    En el Estado 2 el botón 2 sirve para cambiar entre sub - estados, se manera que solo se puede avanzar de estado mediante el
+botón 1.
 '''
 
-# Mensajes de la posición del robot y del wrench
-pose = Pose()
-wrench = WrenchStamped()
-wrench_robot = WrenchStamped()
-st = Int32()
+# Mensajes
+pose = Pose()                       # Posición del extremo del robot
+wrench = WrenchStamped()            # Wrench del Phantom
+wrench_robot = WrenchStamped()      # Wrench del robot
 
-grip_140 = Float64()
+grip_140 = Float64()                # Posición de la pinza de 2 dedos
 
-grip_1 = Float64()
+grip_1 = Float64()                  # Posición de los dedos de la pinza de 3 dedos y la palma
 grip_2 = Float64()
 grip_mid = Float64()
 grip_palm = Float64()
 
 # Factor de escala de los movimientos 
-scale_x = 2
+scale_x = 2                         # Escala del movimiento cartesiano
 scale_y = 2
 scale_z = 8.5
 
-scale_roll = 1
+scale_roll = 1                      # Escala del movimiento en ángulos de Euler
 scale_pitch = 1
 scale_yaw = 1
 
-scale_grip_140 = 4.08
+scale_grip_140 = 4.08               # Escala en el movimiento de las pinzas
 scale_grip = 5
 scale_grip_palm = 0.5
 
+scale_q4 = 0.1                      # Escala en los movimientos articulares de la muñeca del robot
+scale_q5 = 0.1
+scale_q6 = 0.1
+
 # Origen del robot
-or_x = 0.516
-or_y = 0.1334
+or_x = 0.516                        # Origen del movimiento cartesiano
+or_y = 0.1334                       
 or_z = 0.72
 
-or_roll = 0.87
-or_pitch = 1.12
-or_yaw = -0.001661
+or_roll = 0.0 # 0.87                # Origen del movimiento articular de la muñeca del robot
+or_pitch = 0.0 # 1.12
+or_yaw = 0.0 # -0.001661
 
 # Posiciones previas para XYZ y RPY
 #   Para al cambiar de XYZ a RPY y viceversa se guarden las XYZ o RPY segun convenga en el mensaje de pose
@@ -71,9 +79,9 @@ prev_grip_140 = 0
 prev_grip_1, prev_grip_2, prev_grip_mid, prev_grip_palm = 0, 0, 0, 0
 
 # Modos y Flags
-change = True           # Flag para indicar cambio (a True para mandarlo al (0,0,0) al empezar)
-state = 0
-state_3f = 0
+change = True                       # Flag para indicar cambio (a True para mandarlo al (0,0,0) al empezar)
+state = 0                           # Estado del movimiento 
+state_3f = 0                        # Estado del movimiento de la pinza
 
 # Posición actual y previa del Phantom
 prev_pose_phantom = Pose()
@@ -94,21 +102,32 @@ act_pose_phantom.orientation.y = -1.0
 act_pose_phantom.orientation.z = -1.0
 
 # Ganancia del feedback de fuerza
-K = 0
+K = 0                               # Ganancia en el movimiento
 KD = 0.0
 
-Ke = 50
+Ke = 50                             # Ganancia del feedback de fuerza en los cambios de modo
 Kde = 0.1
 
+# Umbral de detección para los cambios
 limit = 0.005
 
-# Tiempo para coger los mensajes del /joint_states
+# Tiempo de espera para /joint_states
 interval = 0.0
 prev = time.time()
 
 # Nombre del robot
 name = "ur5_2"
 grip = "3f"
+
+# Publicar modo en orientación
+pub_orient = []
+
+# Fuerza de la muñeca
+wrist_torque = np.array([0.0, 0.0, 0.0])
+
+# Publisher para el cambio
+pub_change = []
+
 
 # Callback de las posiciones del Phantom
 '''
@@ -135,7 +154,7 @@ EXPLICACIÓN
             También se guarda la ORIENTACIÓN actual de la posición del Phantom
             La posición del robot es la anterior registrada
         
-        - 2.3 ... se almacena la posición en el eje Z del Phantom, correspondiente a la apertura y cierre de la pinza. 
+        - 2.3 ... se almacenan la posiciones del Phantom, correspondiente a la apertura y cierre de cada dedo de la pinza. 
             Se distingue entre dos casos: la pinza de 2 dedos y la de tres dedos en función del nombre asignado al robot.
 '''
 def cb(data):
@@ -197,27 +216,55 @@ def cb(data):
 
 # Callbacks de los botones
 def cb_bt1(data):
-    global change, state, pub_state
+    global change, state
+    global pub_change
 
     if data.buttons[0] == 1:
         state = state + 1       # Incrementa el estado
-        change = True 
+        change = True
 
-        if state > 2:           # Si se pasa del número de estados, ...
-            state = 0           # ... vuelve al estado 0 ...
+        # Si está en modo orientación, publica en el topic a True
+        if state == 1:
+            pub_orient[0].publish(True)
+
+        # Si no, publica False
+        else:
+            pub_orient[0].publish(False)
+
+        # Si se pasa del número de estados, vuelve al estado 0
+        if state > 2:           
+            state = 0 
+
+        pub_change[0].publish(state)
         
 
+# Callback del botón 2
 def cb_bt2(data):
     global change
     global state, state_3f
+    global pub_change
     
+    # Si está en el segundo estado y se presiona el botón
     if state == 2:
         if data.buttons[0] == 1:
+            # Cambia el subestado de la pinza
             state_3f = state_3f + 1
             change = True
-
+            
+            # Si se pasa del número de sub-estados, vuelve al 0
             if state_3f > 1:
                 state_3f = 0
+
+    # Si se está en cualquier otro, vuelve al anterior
+    else:
+        if data.buttons[0] == 1:
+            state = state - 1
+
+            # Si se pasa por debajo de 0, vuelve al máximo
+            if state < 0:
+                state = 2
+
+    pub_change[0].publish(state)
 
 
 # Estado del robot
@@ -225,21 +272,25 @@ def cart_pos(data):
     global prev_x, prev_y, prev_z
     global prev_roll, prev_pitch, prev_yaw
 
+    # Si está en el estado 2, obtiene la posición del extremo
     if state == 0:
         prev_x = data.position.x
         prev_y = data.position.y
         prev_z = data.position.z
 
+    # Si está en el estado 1 obtiene la orientación
     elif state == 1:
         prev_roll = data.orientation.x
         prev_pitch = data.orientation.y
         prev_yaw = data.orientation.z
 
+# Callback de la posición de la pinza
 def grip_pos(data):
     global grip
     global prev_grip_1, prev_grip_2, prev_grip_mid, prev_grip_palm
     global prev_grip_140
 
+    # En función de la pinza se asignan unos valores u otros
     if grip == "2f":
         prev_grip_140 = data.data[0]
 
@@ -249,13 +300,20 @@ def grip_pos(data):
         prev_grip_mid = data.data[2]
         prev_grip_palm = data.data[3]
 
+# Fuerza cartesiana en el extremo del robot
 def cart_force(data):
     global wrench_robot
 
     wrench_robot = data
 
+# Torque de las articulaciones de la muñeca
+def w_torque_cb(data):
+    global wrist_torque
+
+    wrist_torque = data
 
 
+# Main
 if __name__ == "__main__":
 
     if len(sys.argv) > 0:
@@ -275,8 +333,6 @@ if __name__ == "__main__":
         # Nodo
         rospy.init_node(name + "_phantom_ctr")
 
-        pub_state = rospy.Publisher("/" + name + "/state", Int32, queue_size=10)
-
         # Publishers: pose al robot, fuerza al Phantom y modo de movimiento para el robot
         pub = rospy.Publisher("/" + name + "/pose", Pose, queue_size=10)
         pub_f = rospy.Publisher("/" + name + "/servo_cf", WrenchStamped, queue_size=10)
@@ -288,6 +344,10 @@ if __name__ == "__main__":
         rospy.Subscriber('/' + name + '/cart_pos', Pose, cart_pos)
         rospy.Subscriber('/' + name + '/cart_force', WrenchStamped, cart_force)
         rospy.Subscriber("/" + name + "/grip_pos", Float32MultiArray, grip_pos)
+        rospy.Subscriber("/" + name + "/wrist_torque", Float32MultiArray, w_torque_cb)
+        
+        pub_orient.append(rospy.Publisher("/" + name + "/orientation", Bool, queue_size=10))
+        pub_change.append(rospy.Publisher("/" + name + "/change", Int32, queue_size=10))
 
         pub_grip = []
 
@@ -361,7 +421,12 @@ if __name__ == "__main__":
 
 
                 if ex < limit and ey < limit and ez < limit:
-                    change = False        
+                    change = False
+
+                    msg = Int32()
+                    msg.data = -1
+                    
+                    pub_change[0].publish(msg)    
                     print("###################")  
 
                         
@@ -424,12 +489,18 @@ if __name__ == "__main__":
                 k = K
                 kd = KD
 
-            wrench.wrench.force.x = ex * k - ex / (time.time() - t) * kd
-            wrench.wrench.force.y = ey * k - ey / (time.time() - t) * kd
-            wrench.wrench.force.z = ez * k - ez / (time.time() - t) * kd
+            if state != 1:
+                wrench.wrench.force.x = ex * k - ex / (time.time() - t) * kd
+                wrench.wrench.force.y = ey * k - ey / (time.time() - t) * kd
+                wrench.wrench.force.z = ez * k - ez / (time.time() - t) * kd
 
-            if not change:
-                wrench.wrench.force.y = wrench.wrench.force.y + 0.8
+                if not change:
+                    wrench.wrench.force.y = wrench.wrench.force.y + 0.8
+            
+            else:
+                wrench.wrench.force.x = wrist_torque[1] * scale_q5
+                wrench.wrench.force.y = wrist_torque[2] * scale_q6
+                wrench.wrench.force.z = wrist_torque[0] * scale_q4
 
 
             if state == 2 and act_pose_phantom.position.z < 0:
