8fe925be48c3f90ea5bbc521d84ade4a86e800fb
Daniel Frau Alfaro <danielfraualfa@gmail.com>
2023-04-24 18:17:51 +0200

    Update rq_test_sensor.cpp

--- src/robotiq/robotiq_ft_sensor/nodes/rq_test_sensor.cpp
+++ src/robotiq/robotiq_ft_sensor/nodes/rq_test_sensor.cpp
@@ -1,7 +1,7 @@
 /* Software License Agreement (BSD License)
  *
  * Copyright (c) 2014, Robotiq, Inc.
- * All rights reserved.
+ * All rights reserved
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
